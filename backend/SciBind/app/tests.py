a49b63828f6271ec64fe7704c579bf8131e30940
Rishabh Garg <crayzmayz100@gmail.com>
2024-07-25 19:19:32 -0700

    testing: Added testing for event selection and profile pictures

--- backend/SciBind/app/tests.py
+++ backend/SciBind/app/tests.py
@@ -2,7 +2,10 @@ import os
 import csv
 
 from django.test import TestCase
+
 from .models import User, EventModel
+
+from rest_framework.authtoken.models import Token
 
 # Create your tests here.
 
@@ -40,3 +43,49 @@ class EventTest(TestCase):
                     name=row[0], materialtype=row[1], division=row[2]
                 )
                 self.assertTrue(event)
+
+
+class EventSelectionTest(TestCase):
+    def test_event_selection(self):
+        # Create a user
+        user = User.objects.create_user(username="test_user", password="test_password")
+        # Create an event
+        event = EventModel.objects.get(
+            name="Anatomy and Physiology", materialtype="Cheatsheet", division="B"
+        )
+        token = Token.objects.get_or_create(user=user)
+        event_id = event.id
+        response = self.client.post(
+            "/api/event-set/",
+            {
+                "user": user,
+                "events": [event_id],
+            },
+            headers={"Authorization": f"Token {token[0].key}"},
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(user.chosen_events.all()[0].id, event_id)
+        response = self.client.get(
+            "/api/user-events/", headers={"Authorization": f"Token {token[0].key}"}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data[0]["id"], event_id)
+
+
+class ProfilePictureTest(TestCase):
+    def setUp(self):
+        # Create a user
+        self.user = User.objects.create_user(
+            username="test_user", password="test_password"
+        )
+        self.token = Token.objects.get_or_create(user=self.user)
+
+    def test_profile_picture(self):
+        response = self.client.get(
+            "/api/picture/", headers={"Authorization": f"Token {self.token[0].key}"}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response["Content-Disposition"],
+            f"inline; filename={str(self.user.profile_picture).rsplit('\\')[-1]}",
+        )
