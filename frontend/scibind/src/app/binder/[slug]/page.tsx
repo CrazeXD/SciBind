5c04d3cb695fcca572f11e9763789585343a9004
Rishabh Garg <crayzmayz100@gmail.com>
2024-07-27 12:08:07 -0700

    more styling on the document editor

--- frontend/scibind/src/app/binder/[slug]/page.tsx
+++ frontend/scibind/src/app/binder/[slug]/page.tsx
@@ -7,14 +7,20 @@ import Editor from "@/components/editor";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col bg-gray-100">
       <Head>
-        <title>SciBind Editor</title>
+        <title>SciBind</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
-      <Toolbar />
-      <Editor />
+
+      <div className="fixed top-0 w-full z-10">
+        <Header />
+        <Toolbar />
+      </div>
+
+      <div className="mt-28">
+        <Editor />
+      </div>
     </div>
   );
 }
