f88295a7baf8e0404cc55941ea7a88e02c496385
Rishabh Garg <crayzmayz100@gmail.com>
2024-07-26 22:17:37 -0700

    Add online_users field to BinderModel and update dashboard page, create header and toolbar components for editor (still need work)

--- frontend/scibind/src/components/binder.tsx
+++ frontend/scibind/src/components/binder.tsx
@@ -1,23 +1,32 @@
 import { ReactElement } from "react";
 
 interface BinderProps {
+  id: number;
   event: string;
   type: string;
   division: string;
 }
 
-export default function Binder({ event, type, division }: BinderProps): ReactElement {
+export default function Binder({
+  event,
+  type,
+  division,
+  id,
+}: BinderProps): ReactElement {
   return (
     <div className="card bg-base-100 shadow-xl hover:shadow-2xl transition-shadow duration-300">
       <figure className="px-4 pt-4">
         <img
+          // Eventually replace this with a real image
           src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
           alt={`${event} binder`}
           className="rounded-xl object-cover h-48 w-full"
         />
       </figure>
       <div className="card-body">
-        <h2 className="card-title text-2xl font-semibold">{event}</h2>
+        <h2 className="card-title text-2xl font-semibold">
+          <a href={`binder/${id}`}>{event}</a>
+        </h2>
         <div className="flex justify-between items-center mt-2">
           <div>
             <div className="badge badge-secondary p-3 mr-1">{type}</div>
