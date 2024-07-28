b9fea8481ea71392b4d6bdcda835cb04efbbd274
Rishabh Garg <crayzmayz100@gmail.com>
2024-07-28 15:21:50 -0700

    Added tiptap methods

--- frontend/scibind/src/app/binder/[slug]/page.tsx
+++ frontend/scibind/src/app/binder/[slug]/page.tsx
@@ -6,14 +6,19 @@ import Head from "next/head";
 import Header from "@/components/header";
 import Toolbar from "@/components/toolbar";
 import Editor from "@/components/editor";
+import { EditorMethods } from "@/components/editor";
 
 export default function Home() {
+  const [editorMethods, setEditorMethods] = useState<EditorMethods | null>(null);
+  const handleEditorReady = (methods: EditorMethods) => {
+    setEditorMethods(methods);
+  }
+
   const [slug, setSlug] = useState<string | null>(null);
   const pathname = usePathname();
 
   useEffect(() => {
     if (pathname) {
-      // Assuming the slug is the last part of the path
       const pathParts = pathname.split("/");
       const lastPart = pathParts[pathParts.length - 1];
       setSlug(lastPart || null);
@@ -33,7 +38,7 @@ export default function Home() {
       </div>
 
       <div className="mt-28 flex-grow bg-base-200">
-        {slug !== null ? <Editor slug={slug} /> : <div>Loading...</div>}
+        {slug !== null ? <Editor slug={slug} onEditorReady={handleEditorReady}/> : <div>Loading...</div>}
       </div>
     </div>
   );
